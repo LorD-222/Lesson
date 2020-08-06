2496474f9a8342010e191a20b9fe6dc3f474157f
LorD_222 <ilya.r.200594@gmail.com>
2020-08-06 09:52:55 +0300

    commit

--- Functions.py
+++ Functions.py
@@ -3,17 +3,17 @@ import time
 
 def shutdown():
    name_ws = input("Введите имя ПК: ")
-   subprocess.call(["shutdown", "-r","-m", name_ws, "-t", "10"])
+   subprocess.call(["shutdown", "-r","-m", "\\\\" + name_ws, "-t", "10"])
    time.sleep(3)
 
 def restart_spooler():
    name_ws = input("Введите имя ПК:")
-   subprocess.call(['sc',name_ws, 'stop', 'PDF24'])
+   subprocess.call(['sc',"\\\\" + name_ws, 'stop', 'PDF24'])
    time.sleep(3)
-   subprocess.call(['sc',name_ws, 'stop', 'Spooler'])
-   subprocess.call(['sc',name_ws, 'start', 'Spooler'])
+   subprocess.call(['sc',"\\\\" + name_ws, 'stop', 'Spooler'])
+   subprocess.call(['sc',"\\\\" + name_ws, 'start', 'Spooler'])
    time.sleep(3)
-   subprocess.call(['sc',name_ws, 'start', 'PDF24'])
+   subprocess.call(['sc',"\\\\" + name_ws, 'start', 'PDF24'])
 
 def stop_service():
    name_ws = input("Введите имя ПК:")
